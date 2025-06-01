d79c92e210b3aa73fe5dbb854b5ce43f9db226db
Ibrahim H. Yazouri <ibrahimhyaz@gmail.com>
2025-06-01 07:30:09 +0200

    build 🧩: impelment user logout

--- app.vue
+++ app.vue
@@ -16,4 +16,20 @@
 * {
   font-family: 'Inter', serif;
 }
+
+::-webkit-scrollbar {
+  width: 8px;
+}
+
+::-webkit-scrollbar-track {
+  background: #0a0a0a;
+  border-radius: 10px;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #3f3f46;
+  border-radius: 10px;
+  border: 2px solid transparent;
+  background-clip: content-box;
+}
 </style>
